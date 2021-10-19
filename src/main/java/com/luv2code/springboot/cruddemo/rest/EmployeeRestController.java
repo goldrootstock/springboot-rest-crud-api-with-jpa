2bdbea6e1a290fac21c2d1fb473d1f8472dae772
juwonkim <goldrootstock@gmail.com>
2021-10-19 20:41:59 +0900

    REST Controller Methods to Update and Delete

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +48,7 @@ public class EmployeeRestController {
 	
 	// add mapping for POST /employees - add new employee
 	
-	@PostMapping("/employeee")
+	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee theEmployee) {
 		
 		// also just in case they pass an id in JSON ... set id to 0
@@ -59,4 +61,36 @@ public class EmployeeRestController {
 		return theEmployee;
 		
 	}
+	
+	// add mapping for PUT /employees - update existing employee
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+		
+	}
+	
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		if(tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
+		
+	}
+	
+	
+	
+	
 }
